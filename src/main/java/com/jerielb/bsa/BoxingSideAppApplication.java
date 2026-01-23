852a2685b83010c8da2d9d1aa33684b14a2a3ab2
jerielB <jerielbongco98@gmail.com>
2026-01-23 18:14:13 +1100

    initial commit

---
+++ src/main/java/com/jerielb/bsa/BoxingSideAppApplication.java
@@ -0,0 +1,13 @@
+package com.jerielb.bsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxingSideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxingSideAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerielb/bsa/BoxingSideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerielb.bsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxingSideAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
