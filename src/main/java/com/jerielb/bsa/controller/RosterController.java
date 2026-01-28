c18f1e4ad64b878b247f978a8334847c78d2267c
jerielB <jerielbongco98@gmail.com>
2026-01-28 13:20:25 +1100

    layout V1

--- src/main/java/com/jerielb/bsa/controller/RosterController.java
+++ src/main/java/com/jerielb/bsa/controller/RosterController.java
@@ -1,14 +1,25 @@
 package com.jerielb.bsa.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RosterController {
+	private final Logger LOGGER = LogManager.getLogger(RosterController.class);
+	
+	@RequestMapping(path="/index", method= RequestMethod.GET)
+	public String getHomePage() {
+		LOGGER.info("Redirecting to Home page");
+		return "redirect:/";
+	}
 	
 	@RequestMapping(path="/roster", method= RequestMethod.GET)
-	public String getRosterPage() {
+	public String getRosterPage(Model model) {
+		LOGGER.info("Redirecting to Roster page");
 		return "roster";
 	}
 }
