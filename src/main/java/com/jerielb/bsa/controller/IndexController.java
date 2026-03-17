8272537355ac8342a4dc16d6324ed9d706e498b2
jerielB <jerielbongco98@gmail.com>
2026-03-17 13:47:40 +1100

    1.1 - added @Controller annotation to  IndexController.java

--- src/main/java/com/jerielb/bsa/controller/IndexController.java
+++ src/main/java/com/jerielb/bsa/controller/IndexController.java
@@ -2,9 +2,11 @@ package com.jerielb.bsa.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class IndexController {
 	private final Logger LOGGER = LogManager.getLogger(RosterController.class);
 	
